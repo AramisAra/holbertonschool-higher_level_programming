79c558104e52df8b5fc7fcbee79d7fda5c8c51b3
Aramis Martinez <martinezcruzaj@gmail.com>
2024-01-30 12:46:07 -0400

    Adding work of task 8, and  9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    for key, value in a_dictionary.items():
+        a_dictionary[key] = value * 2
+    new_dict = a_dictionary
+    return new_dict
