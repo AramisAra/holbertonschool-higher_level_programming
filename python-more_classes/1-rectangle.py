4d13458c8f4a2fb003fae68ea2881cac478cd62d
Aramis Martinez <martinezcruzaj@gmail.com>
2024-02-05 18:28:10 -0500

    More Fixes

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -24,27 +24,30 @@ class Rectangle:
         """
         This is a docstring for the height setter method.
         """
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+
+        self.__height = value
 
     def width(self):
         """
         This is a docstring for the width method.
         this returns self.__width
         """
+
         return self.__width
 
     def width(self, value):
         """
         This is a docstring for the width setter method.
         """
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+
+        self.__width = value
