08f0b5828285c9d7ad71d0ebd62fb8d5f9088260
Aramis Martinez <martinezcruzaj@gmail.com>
2024-02-05 18:03:47 -0500

    Adding the code for task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,1 +1,48 @@
 #!/usr/bin/python3
+"""
+This is a docstring for the Rectangle class.
+"""
+
+class Rectangle:
+    """
+    This is a docstring for the Rectangle class.
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def width(self):
+        """
+        This is a docstring for the width method.
+        this returns self.__width
+        """
+        return self.__width
+
+    def width(self, value):
+        """
+        This is a docstring for the width setter method.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    def height(self):
+        """
+        This is a docstring for the height method.
+        """
+        return self.__height
+
+    def height(self, value):
+        """
+        This is a docstring for the height setter method.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
