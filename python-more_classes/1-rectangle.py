cde3623232180861a263108c29ea22910a82bb9a
Aramis Martinez <martinezcruzaj@gmail.com>
2024-02-05 18:24:29 -0500

    Adding the fixes

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,8 +8,31 @@ class Rectangle:
     This is a docstring for the Rectangle class.
     """
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        """
+        This adding the need atributtes
+        """
+        self.__width = width
+        self.__height = height
+
+    def height(self):
+        """
+        This is a docstring for the height method.
+        """
+        temp = self.__height
+        return temp
+
+    def height(self, value):
+        """
+        This is a docstring for the height setter method.
+        """
+        height = height(self)
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            height = value
 
     def width(self):
         """
@@ -22,27 +45,11 @@ class Rectangle:
         """
         This is a docstring for the width setter method.
         """
+        width = width(self)
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-
-        self.__width = value
-
-    def height(self):
-        """
-        This is a docstring for the height method.
-        """
-        return self.__height
-
-    def height(self, value):
-        """
-        This is a docstring for the height setter method.
-        """
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-
-        if value < 0:
-            raise ValueError("height must be >= 0")
-
-        self.__height = value
+        else:
+            width = value
