ab9c4087925ad1a8d581a3f8efd47d379acedf33
Aramis Martinez <martinezcruzaj@gmail.com>
2024-02-05 21:56:10 -0500

    Adding fixes

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-def copy_list(l):
-    """
-    Copy a list.
-
-    Args:
-        l (list): The list to be copied.
-
-    Returns:
-        list: A copy of the input list.
-    """
-    return l.copy()
+def copy_list(a_list):
+    return a_list.copy()
