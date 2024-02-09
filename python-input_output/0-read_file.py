dc5de1806bf8175997d20e17c61a65d9c675f4a3
Aramis Martinez <martinezcruzaj@gmail.com>
2024-02-09 07:27:30 -0500

    Adding the code for the input output project

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""0. Read file """
+def read_file(filename=""):
+    """
+    Reads and prints the contents of a file.
+
+    Args:
+        filename (str): The name of the file to be read.
+
+    Returns:
+        None
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end="")
