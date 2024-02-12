73a99f85605d5b748ce6538dd71b875e6ccb9ec7
Aramis Martinez <martinezcruzaj@gmail.com>
2024-02-12 18:40:00 -0500

    Adding the base class to test

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+class Base():
+    
+    self.__nb_objects = 0
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+            self.__nb_objects = id
+        elif id == None:
+            self.__nb_objects += 1
