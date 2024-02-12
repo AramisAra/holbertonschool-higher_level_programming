5d7b7addeb458fe233eb2d5d5b40d5dbb71bc0e9
Aramis Martinez <martinezcruzaj@gmail.com>
2024-02-12 18:43:27 -0500

    Adding the doc to the class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 
 class Base():
-    
+    """
+    Base class for all other classes in the project.
+    """
+
     self.__nb_objects = 0
     def __init__(self, id=None):
+        """
+        Initializes a new instance of the Base class.
+
+        Args:
+            id (int): The id of the object. Defaults to None.
+        """
         if id != None:
             self.id = id
             self.__nb_objects = id
