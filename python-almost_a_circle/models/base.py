9a9ae924e4dd06b6a23791d0aa033ec5cfbc9866
Aramis Martinez <martinezcruzaj@gmail.com>
2024-02-12 18:59:46 -0500

    Adding base change to test if it pass checkers

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,7 +6,7 @@ class Base():
     Base class for all other classes in the project.
     """
 
-    self.__nb_objects = 0
+    __nb_objects = 0
     def __init__(self, id=None):
         """
         Initializes a new instance of the Base class.
@@ -14,8 +14,10 @@ class Base():
         Args:
             id (int): The id of the object. Defaults to None.
         """
+
+        type(self).__nb_objects += 1
+        self.id = type(self).__nb_objects
         if id != None:
             self.id = id
-            self.__nb_objects = id
-        elif id == None:
-            self.__nb_objects += 1
+            __nb_objects = id
+            type(self).__nb_objects -= 1
