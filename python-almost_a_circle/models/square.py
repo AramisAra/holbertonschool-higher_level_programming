63389dddf1b76b03e44f4967bc8a9c8b0b8f8d70
Aramis Martinez <martinezcruzaj@gmail.com>
2024-02-13 20:47:39 -0500

    Adding the doc to the class square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ Class Square """
+
 from models.rectangle import Rectangle
 
 class Square(Rectangle):
+    """ Class Square """
+    
     def __init__(self, size, x=0, y=0, id=None):
         """
         Initialize a Square instance.
