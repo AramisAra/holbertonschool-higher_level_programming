cd682916a7c5ccfa97f9efd166e84275593808c5
Aramis Martinez <martinezcruzaj@gmail.com>
2024-02-13 23:24:26 -0500

    Adding a "s"

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -51,7 +51,7 @@ class Base():
     @staticmethod
     def from_json_string(json_string):
         if json_string is None or json_string == "[]":
-            return "[]"
-        return json.load(json_string)
+            return []
+        return json.loads(json_string)
 
 
