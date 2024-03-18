4e2ba0f29f0ccd43e7866333175934dc56591730
AramisAra <martinezcruzaj@gmail.com>
2024-03-18 05:25:49 -0400

    Test different code

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,48 +1,17 @@
 #!/usr/bin/python3
-""" Import for the file """
+"""
+Lists all states from the database hbtn_0e_0_usa
+"""
 import sys
 import MySQLdb
 
-def state_list(user, passwd, data):
-    """
-    Connects to a MySQL database and prints out a list of states ordered by their ID.
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
-    Parameters:
-    user (str): The username for the MySQL database
-    passwd (str): The password for the MySQL database
-    data (str): The name of the database to connect to
-    
-    Returns:
-    States
-    """
-
-    # Connection to database
-    db = MySQLdb.connect(host='loaclhost', port='3306',
-    user=user, passwd=passwd , db=data)
-
-    # This is a cursor to be able to make query use python
     cur = db.cursor()
-
-    # This is the query to get the table
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # This is to fetch the table to print it
+    cur.execute("SELECT * FROM states;")
     states = cur.fetchall()
 
-    # This is to print the fetched rows
     for state in states:
         print(state)
-
-    cur.close()
-    db.close()
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database>")
-        sys.exit(1)
-
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    data = sys.argv[3]
-
-    state_list(user, passwd, data)
