6c63b2b44bbc84632db3da72706f040aa7be8e1a
AramisAra <martinezcruzaj@gmail.com>
2024-03-19 10:10:48 -0400

    Adding binary to query

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 
     cur.execute(f"SELECT * \
                 FROM states \
-                WHERE name LIKE '{sys.argv[4]}';")
+                WHERE BINARY name LIKE '{sys.argv[4]}';")
 
     states = cur.fetchall()
     for state in states:
