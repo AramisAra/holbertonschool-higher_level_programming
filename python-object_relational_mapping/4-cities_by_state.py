ff5aaaff613ffb0458ef1acb26cdf43d74ef2ef6
AramisAra <martinezcruzaj@gmail.com>
2024-03-22 19:00:14 +0000

    Fixing changes

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -19,9 +19,9 @@ if __name__ == '__main__':
     cur = db.cursor()
 
     # Executing MySql Query
-    cur.execute("SELECT cities.id, cities.name,\
-                states.name FROM cities JOIN states ON\
-                cities.state_id = states.id ORDER BY cities.id")
+    cur.execute("SELECT cities.id, cities.name, \
+                states.name FROM cities JOIN states ON \
+                cities.state_id = states.id")
 
     # Obtaining Query Result & prints the result in rows
     rows = cur.fetchall()
