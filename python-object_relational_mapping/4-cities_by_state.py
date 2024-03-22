a999b65188fc93acc664c99f9e4686adc545c6b7
AramisAra <martinezcruzaj@gmail.com>
2024-03-22 16:10:36 +0000

    Adding fixes of task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -2,13 +2,13 @@
 """script that lists all cities with the state"""
 import MySQLdb as sql
 from sys import argv
-
+3
 if __name__ == '__main__':
     db = sql.connect(host="localhost", port=3306,
                      user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    query = "SELECT cities.id, cities.name,\
-    states.name FROM cities LEFT JOIN states\
+    query = "SELECT cities.id, cities.name, \
+    states.name FROM cities LEFT JOIN states \
     ON cities.state_id = states.id;"
     cur.execute(query)
     rows = cur.fetchall()
